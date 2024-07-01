ba970d2471d9d69b89081c105d9bfb71509cc5f4
Webster <weixiao.chen.98@foxmail.com>
2024-07-02 00:16:49 +0800

    creating the TicketList component and conditional rendering

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TicketItem from "./TicketItem";
+
+const TicketList = ({tickets, dispatch}) => {
+    return (
+        <div className="ticket-list">
+            {tickets.map(ticket => <TicketItem key={ticket.id} ticket={ticket} dispatch={dispatch} />)}
+        </div>
+    )
+}
+export default TicketList
