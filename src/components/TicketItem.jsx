db01898931ca0a8bf54fc5d3aa5c1132330718c9
Webster <weixiao.chen.98@foxmail.com>
2024-07-02 23:14:23 +0800

    deleting tickets

--- src/components/TicketItem.jsx
+++ src/components/TicketItem.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 
 const TicketItem = ({ticket, dispatch}) => {
     const {id, title, description, priority} = ticket;
+
     const priorityClass = {
         1: "priority-low",
         2: "priority-medium",
         3: "priority-high"
     }
+
     return (
         <div className="ticket-item">
             <div className={`priority-dot ${priorityClass[priority]}`}></div>
+
             <h3>{title}</h3>
             <p>{description}</p>
+
+            <button className="button" onClick={() => dispatch({type: "DELETE_TICKET", payload: {id: id}})}>
+                Delete
+            </button>
         </div>
     )
 }
